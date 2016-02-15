28ee2491c606ef581704a97dabc9c145da8f5b3a
Gregor Müllegger <gregor@muellegger.de>
2016-02-15 18:44:41 +0100

    Test for global exports

---
+++ tests/test_global_exports.py
@@ -0,0 +1,19 @@
+EXPECTED_EXPORTS = (
+    'ForeignKeyFormField',
+    'FormField',
+    'FormSetField',
+    'FormSetWidget',
+    'FormWidget',
+    'InlineFormSetField',
+    'ModelFormField',
+    'ModelFormSetField',
+    'SuperForm',
+    'SuperModelForm',
+)
+
+
+def test_all_exports_are_there():
+    import django_superform
+
+    for expected_export in EXPECTED_EXPORTS:
+        assert hasattr(django_superform, expected_export)
