2171536ad3a7418aa1c0067fcafb7d25126e89ee
Lucas Wiman <lucaswiman@counsyl.com>
2016-12-28 17:15:47 -0800

    Add new required TEMPLATES setup.

--- tests/settings.py
+++ tests/settings.py
@@ -21,3 +21,22 @@ MIDDLEWARE_CLASSES = ()
 STATIC_URL = '/static/'
 
 SECRET_KEY = '0'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ],
+        },
+    },
+]
